b058badb67ef2031be815ec9f6d5ed25df686a28
taulantn <tn47423@ubt-uni.net>
2022-10-25 12:21:35 +0200

    + Cart and Order cnrtrllrs and blades, admin dash.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])-> name('homepage');
 Route::get('/shop', [\App\Http\Controllers\ShopController::class, 'index'])-> name('shop.index');
 Route::get('/product', [\App\Http\Controllers\ProductController::class, 'show'])-> name('product.show');
+
+Route::resource('/cart', \App\Http\Controllers\CartController::class);
+
+Route::get('/order/checkout', [\App\Http\Controllers\OrderController::class, 'process'])->name('checkout.process');
+
+Route::get('/admin', [\App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
